b93d59b606f8f2d4da9f815d40f596e31117645b
hisbullapvt <hisbullah.pvt@gmail.com>
2026-07-19 18:10:01 +0530

    Updated init source code 1

---
+++ init.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+    execve("/system/bin/init", argv, envp);
+
+    // execve fail aana mattum inga varum
+    while (1) {
+        sleep(1);
+    }
+
+    return 0;
+}
